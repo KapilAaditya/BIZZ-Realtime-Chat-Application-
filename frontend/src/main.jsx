570ad1f6dc5dc37f68717807a0fd487d1f196ae4
Aaditya Kapil <aadityakapil2006@gmail.com>
2026-07-03 21:44:25 +0530

    Fix import statement for BrowserRouter to use 'react-router-dom'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/react";
-import { BrowserRouter } from "react-router";
-
+import { BrowserRouter } from 'react-router-dom'; //  CORRECT
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
