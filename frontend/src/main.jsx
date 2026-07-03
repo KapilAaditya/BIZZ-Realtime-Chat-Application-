0bc01365f4ad1e4da2c9122cbcb292b852bb1a78
Aaditya Kapil <aadityakapil2006@gmail.com>
2026-07-03 21:54:39 +0530

    more 3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/react";
-import { BrowserRouter } from 'react-router-dom'; //  CORRECT
+import { BrowserRouter } from "react-router";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
