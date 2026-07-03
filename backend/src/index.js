d1e432f103faf4ab6aec4e07e5f7589cda3e8070
Aaditya Kapil <aadityakapil2006@gmail.com>
2026-07-03 22:52:19 +0530

    more 15

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,8 +36,10 @@ if (fs.existsSync(publicDir)) {
 
   // Fix: Corrected the catch-all routing syntax from '/{*any}' to '*'
   //  This syntax is perfectly valid for modern wildcards
-app.get("/{*any}", (req, res, next) => {
-    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+app.get("/:any*", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => {
+      if (err) next(err);
+    });
   });
 }
 
