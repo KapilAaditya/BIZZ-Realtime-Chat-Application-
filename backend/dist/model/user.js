bfb1bf99e082fc64cb48209a2124fbc90bbfd3f5
Aaditya Kapil <aadityakapil2006@gmail.com>
2026-07-04 20:16:40 +0530

    day 2 5

--- backend/dist/model/user.js
+++ backend/dist/model/user.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    clerkid: {
+    clerkId: { // <-- Changed from clerkid to clerkId (Capital I)
         type: String,
         required: true,
         unique: true
-    }, fullName: {
+    }, 
+    fullName: {
         type: String,
         required: true,
     },
     email: {
         type: String,
         required: true,
-        unique : true,
+        unique: true,
     },
     profilePic: {
         type: String,
         default: ""
     }
-}, { timestamps: true },)// yeh btata hai kab a user bana or kab update hua  genrated automaticall by mongoos
-const User = mongoose.model('User', userSchema)
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
